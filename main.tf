9996c726b8123683f23e803a582d8ec4dc0027a5
yand <yanivdn@gmail.com>
2023-05-01 18:07:56 -0700

    apply security modlule

--- main.tf
+++ main.tf
@@ -22,11 +22,19 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
     Name = "HelloWorld"
   }
+}
+
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.17.2"
+  name = "blog_new"
+
+  vpc_id = data.aws_vpc.default.id
 }
 
 resource "aws_security_group" "blog" {
@@ -34,6 +42,12 @@ resource "aws_security_group" "blog" {
   description = "Allow http and https in. Allow everything out"
 
   vpc_id = data.aws_vpc.default.id
+
+  ingress_rules = ["http-80-tcp", "https-443-tcp"]
+  ingress_cidr_blocks = [0.0.0.0/0]
+
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = [0.0.0.0/0]
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
