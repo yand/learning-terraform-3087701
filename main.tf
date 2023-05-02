5423013849f080492eea06a6c71c21fb45c4b70b
yand <yanivdn@gmail.com>
2023-05-01 18:30:49 -0700

    rollback

--- main.tf
+++ main.tf
@@ -14,21 +14,8 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = "dev"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  enable_nat_gateway = true
-
-  tags = {
-    Terraform = "true"
-    Environment = "dev"
-  }
+data "aws_vpc" "default" {
+  default = true
 }
 
 resource "aws_instance" "blog" {
@@ -45,9 +32,9 @@ resource "aws_instance" "blog" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.17.2"
-  
   name = "blog"
-  vpc_id = module.vpc.public_subnets[0]
+
+  vpc_id = data.aws_vpc.default.id
 
   ingress_rules = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
