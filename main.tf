daa3c29a75ee28ce359b0c2993d51f571329595c
yand <yanivdn@gmail.com>
2023-05-01 18:11:12 -0700

    fix wrong line

--- main.tf
+++ main.tf
@@ -35,6 +35,12 @@ module "blog_sg" {
   name = "blog_new"
 
   vpc_id = data.aws_vpc.default.id
+
+  ingress_rules = ["http-80-tcp", "https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group" "blog" {
@@ -42,12 +48,6 @@ resource "aws_security_group" "blog" {
   description = "Allow http and https in. Allow everything out"
 
   vpc_id = data.aws_vpc.default.id
-
-  ingress_rules = ["http-80-tcp", "https-443-tcp"]
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-
-  egress_rules = ["all-all"]
-  egress_cidr_blocks = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
