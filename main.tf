9bec83ad205b3a2e5c4acede28c8d874e1382bf0
yand <yanivdn@gmail.com>
2023-05-01 18:26:25 -0700

    typo

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ module "vpc" {
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["eu-west-2a", "eu-west-21b", "eu-west-2c"]
+  azs             = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true
