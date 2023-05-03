fa1f24deb3215a97533443805a180b7f3acbe622
yand <yanivdn@gmail.com>
2023-05-02 17:44:39 -0700

    first sn

--- main.tf
+++ main.tf
@@ -47,7 +47,7 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
 
-  vpc_id  = module.blog_vpc.vpc_id
+  vpc_id  = module.blog_vpc.vpc_id[0]
   name    = "blog"
   ingress_rules = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
